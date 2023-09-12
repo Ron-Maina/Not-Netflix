69f7e6900524a5e22b98dd91a4e102b191e2078f
Ron Maina <ronmwangi00@gmail.com>
2023-09-12 15:57:00 +0300

    Added navbar to app

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar() {
+
+  return (
+    <div style={{width: "100%"}}>
+        <nav style={{padding: "20px", textAlign: "center", backgroundColor: "black"}}>
+            <h1 id='title'style={{color: "#800020"}}>
+                Not-Netflix
+            </h1>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
