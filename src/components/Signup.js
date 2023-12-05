2b1a8f438112ef167e116ff68cda351b5c6a7f51
Ron Maina <ronmwangi00@gmail.com>
2023-12-06 01:59:59 +0300

    User signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useGoogleLogin } from '@react-oauth/google';
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
@@ -11,59 +12,76 @@ function Signup(){
     const [Username, setUsername] = useState("")
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
+    const [confirmed_password, setconfirmed_password] = useState("")
 
-    const [checkEmail, setCheckEmail] = useState([])
-    const [checkUsername, setCheckName] = useState([])
+    const [showPassword, setShowPassword] = useState(false);
 
+    const [successfulSignup, setSuccessfulSignup] = useState(false); 
+    const [signUpFailed, setSignUpFailed] = useState(false); 
+
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+      });
 
     function handleSubmit(e){
         e.preventDefault()
         const userDetails = {
             "username": Username,
             "email": Email,
-            "password": Password
+            "password": Password,
         }
         addToLogins(userDetails)
         setUsername("")
         setEmail("")
         setPassword("")
+        setconfirmed_password("")
     }
 
     function loginPage(){
         navigate("/login")
+        setSuccessfulSignup(false);
     }
 
     function addToLogins(details){
-        if (checkEmail.includes(details.email)){
-            alert("Email already in use")
-        }
-        else if (checkUsername.includes(details.username)){
-            alert("Username already in use")
-        }
-        else{
-            fetch('http://localhost:3001/userDetails', {
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(details)
-            })
-            alert("Sign Up Successful")
-            setCheckEmail([...checkEmail, Email])
-            setCheckName([...checkUsername, Username])
-        }
-        loginPage()
+        fetch('http://127.0.0.1:5000/api/users', {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(details)
+        })
+        .then (res => res.json())
+        .then(data => {
+            if (data[1] === 201){
+                setSuccessfulSignup(true);
+                loginPage()
+            }
+            else {
+                setSignUpFailed(true)
+                setTimeout(() => {
+                    setSignUpFailed(false)
+                }, 2000)
+                
+            }
+        })
+        
+        
     }
 
     return (
-        <>
+        <div className='page'>
+            <div id="signup-bg"></div>
+            <>
             <NavBar />
             <div id = "signupPage" className='login-details'>
+                <h3 style={{fontFamily: "serif", paddingBottom: '15px'}}>Sign Up</h3>
                 <form onSubmit={handleSubmit}>
                     <div id = "username">
-                        <label htmlFor='username'>Username: </label>
+                        <label htmlFor='username'></label>
                         <input
+                        size={30}
+                        placeholder='Username'
                         name='username'
                         required
                         type='text'
@@ -73,8 +91,10 @@ function Signup(){
                     </div>
                     <br/>
                     <div id = "email">
-                        <label htmlFor='email'>Email: </label>
+                        <label htmlFor='email'></label>
                         <input
+                        size={30}
+                        placeholder='Email'
                         name='email'
                         required
                         type='email'
@@ -84,28 +104,67 @@ function Signup(){
                     </div>
                     <br/>
                     <div id = "password">
-                        <label htmlFor='password'>Password: </label>
+                        <label htmlFor='password'> </label>
                         <input
+                        size={30}
+                        placeholder='Password'
                         name='password'
+                        autoComplete='off'
                         required
-                        type='text'
                         minLength="5"
+                        type={showPassword ? 'text' : 'password'}
                         value={Password}
                         onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <br/>
-                    <div style={{textAlign:"center"}}>
-                        <Button variant="danger" type='submit'>Signup</Button>{' '}  
+                    <div id = "password">
+                        <label htmlFor='password'> </label>
+                        <input
+                        size={30}
+                        placeholder='Confirm Password'
+                        name='password-confirmation'
+                        autoComplete='off'
+                        required
+                        minLength="5"
+                        type={showPassword ? 'text' : 'password'}
+                        value={confirmed_password}
+                        onChange={(e) => setconfirmed_password(e.target.value)}
+                        />
+                    </div>
+                    <div style={{textAlign: 'left'}}>
+                        <label>
+                            <input 
+                            type="checkbox" 
+                            onChange={() => setShowPassword(!showPassword)} 
+                            style={{verticalAlign: 'middle', margin: '10px'}}/>
+                            Show password
+                        </label>
+                    </div>
+                    
+                    <div style={{textAlign:"center", paddingTop: '20px'}} className="d-grid gap-2">
+                        <Button variant="danger" type='submit' size='lg'>Signup</Button>{' '} 
                     </div>
                 </form>
+
+                {successfulSignup && (
+                    <p style={{ color: 'white', textAlign: 'center' }}>Successful signup</p>
+                )}
+                {signUpFailed && (
+                    <p style={{ color: 'red', textAlign: 'center' }}>SignUp Failed! Email may already exist</p>
+                )}
+
                 <br/>
-                <div>
-                    <span>Already have an account?</span> 
-                    <Link to = "/login"><Button variant="danger" type='submit'>Login</Button>{' '} </Link>  
+                <h4 style={{fontSize: '1em'}}>OR</h4>
+                <div style={{textAlign:"center", paddingTop: '10px'}} className="d-grid gap-2">
+                    <Button variant="outline-light" type='submit' onClick={() => login()}>Sign up with Google 🚀</Button>{' '} 
+                </div>
+                <div style={{display: 'flex', gap: '10px', paddingTop: '30px'}}>
+                    <span>Already have an account? <Link to = "/login" style={{textDecoration: 'none', color: "red"}}>Login</Link></span> 
                 </div>
             </div>
-        </>
+            </>
+        </div>
     )
 }
 
