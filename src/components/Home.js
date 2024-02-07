8e0d56e1879be12451cb8b4a32ace03c1d0483d9
Ron Maina <ronmwangi00@gmail.com>
2024-02-07 16:42:15 +0300

    Search filter to display only searched movie

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,28 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import 'react-slideshow-image/dist/styles.css'
-import { Slide } from 'react-slideshow-image';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { CCarousel } from '@coreui/react'
-import { CCarouselItem } from '@coreui/react'
-import { CImage } from '@coreui/react'
-import { CCarouselCaption } from '@coreui/react'
+import {FaStar} from "react-icons/fa";
+import {GoDotFill} from "react-icons/go";
+import { FaPlay } from "react-icons/fa";
+import { FiPlusCircle } from "react-icons/fi";
+import { AiOutlineClose } from 'react-icons/ai';
 
 
 
-
-
-import Sidebar from './Sidebar'
+import Sidebar from './Sidebar';
 import NavBar from './NavBar';
-import {useNavigate} from "react-router-dom"
+import Carousel from './Carousel';
+import SearchBar from './SearchBar'
 
 
-function Home({onRenderDetails}) {
 
-  const [Trending, setTrending] = useState([])
-  const [Movies, setMovies] = useState([])
-  const [All, setAll] = useState([])
 
+function Home({onRenderWatchlist}) {
+ 
   const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
 
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/account/20421072/favorite/movies?language=en-US&page=2&sort_by=created_at.asc', {
-      method: 'GET',
-      headers: {
-        "Accept": 'application/json',
-        Authorization: process.env.REACT_APP_API_KEY
-      }
-    })
-    .then(res => res.json())
-    .then(data => setMovies(data.results)) 
-  }, [])
-
+  const [Display, setDisplay] = useState([])
   useEffect(() => {
     fetch("https://api.themoviedb.org/3/trending/all/day?language=en-US", {
       method: 'GET',
@@ -47,83 +32,205 @@ function Home({onRenderDetails}) {
       }
     })
     .then(res => res.json())
-    .then(data => setAll(data.results)) 
+    .then(data => setDisplay(data.results)) 
   }, [])
-
   
-  // function viewDetails(movie_id = 0, show_id = 0){
-  //   if (movie_id != 0){
-  //     const filteredMovie = Movies.filter(movie => movie.id === movie_id)
-  //     navigate(`/movies/${movie_id}`)
-  //     onRenderDetails(filteredMovie)
-  //   }
-  //   else if (show_id != 0){
-  //     const filteredShow = TvShows.filter(show => show.id === show_id)
-  //     navigate(`/movies/${show_id}`)
-  //     onRenderDetails(filteredShow)
-  //   }
 
-  // }
+  const [activeCategory, setactiveCategory] = useState('all');
+  const filterByCategory = (word) => {
+    setactiveCategory(word);
+    fetch(`https://api.themoviedb.org/3/trending/${word}/day?language=en-US`, {
+      method: 'GET',
+      headers: {
+        "Accept": 'application/json',
+        Authorization: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(res => res.json())
+    .then(data => setDisplay(data.results)) 
+  };
 
+
+  const [activeFilter, setactiveFilter] = useState('');
+  const filters = (filter) => {
+    setactiveFilter(filter);
+    
+    if (activeCategory === 'all') return null;
+  
+    let endpoint;
+  
+    if (activeCategory === 'tv') {
+      endpoint = (filter === 'new' || filter === 'upcoming') ? (filter === 'new' ? 'airing_today' : 'on_the_air') : filter;
+    } else if (activeCategory === 'movie') {
+      endpoint = filter === 'new' ? 'now_playing' : filter;
+    } 
+   
+    fetch(`https://api.themoviedb.org/3/${activeCategory}/${endpoint}?language=en-US&page=1`, {
+      method: 'GET',
+      headers: {
+        "Accept": 'application/json',
+        Authorization: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(res => res.json())
+    .then(data => setDisplay(data.results));
+  };
+
+  const [movieDetails, setMovieDetails] = useState('')
+  const [carouselVisible, setCarouselVisible] = useState(true);
+
+  function MovieDetails(film){
+    setCarouselVisible(false);
+    setMovieDetails(film)
+  }
+
+  const [videoId, setVideoId] = useState('');
+  const [showVideo, setShowVideo] = useState(false);
+  let videoData
+
+  function PlayVideo(title){
+      console.log(title)
+      let url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCGVPxVpmiOLtlubbjXXzIkZNjoyjPoL_w&q=${title} trailer&part=snippet&maxResults=1`
+      fetch(url, {
+        method: 'GET',
+        headers : {
+          'Content-Type': 'application/json',  
+        }  
+      })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      videoData = data.items[0].id.videoId
+      renderVideo(videoData)
+      setVideoId(videoData)
+    })
+    
+    function renderVideo(videoData){
+      console.log(videoData)
+
+      if (videoData){
+        setShowVideo(true)
+        console.log(videoId)
+      }
+      else{
+        alert("Error playing Video")
+      }
+      console.log(videoId)
+    }
+    
+  }
+
+  function EndVideo(){
+    setShowVideo(false);
+  };
+  
+  const filtered_display = Display.filter(movie => {
+    return (movie.title || movie.name).toLowerCase().includes(searchTerm.toLowerCase())
+  })
+  
 
   return (
     <>
-      <div style={{ display: 'grid', gridTemplateColumns: "auto 1fr"}} className='app-page'>
+      <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr' }} className='app-page'>
         <Sidebar />
-        <div style={{height: "100vh", overflow: "hidden"}} >
 
-          <NavBar/>
-          <CCarousel className='carousel'>
-          {Movies.map(movie => (
-            <CCarouselItem>
-              <CImage className="d-block w-100" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="slider" />
-              <CCarouselCaption className="d-none d-md-block" id='carousel-caption'>
-                  <h3 style={{fontFamily: 'fantasy'}}>{movie.title}</h3>
-                  <br/>
-                  <p>{movie.overview}</p>
-              </CCarouselCaption>
-            </CCarouselItem>
-          ))}
-          </CCarousel>
-         
+        <div style={{height: "100vh", overflow: "hidden"}} >
+          <div className='home-nav'>
+            <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+            <NavBar/>
+          </div>
+
+          <div>
+            {carouselVisible && (
+              <div> <Carousel renderTrailer={PlayVideo} /> </div>
+            )}
+            {!carouselVisible && (
+              <>
+                {movieDetails && (
+                  <div id='selected_movie'
+                  style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movieDetails.poster_path})`, backgroundSize: "fill",
+                  backgroundPosition: 'center', filter: 'brightness(0.7)'}}>
+                    <div style={{marginLeft: '50px', paddingTop: '100px', color: 'white'}}>
+                      <div style={{display: 'flex', gap:'15px'}}>
+                        <p><FaStar style={{ color: 'red', marginRight:'5px', marginTop: '-5px'}}/> {movieDetails.vote_average} </p>
+                        <GoDotFill style={{fontSize: '10px', marginTop: '7px', color: 'gray'}}/>
+                        <p>{movieDetails.release_date || movieDetails.first_air_date}</p>
+                        <GoDotFill style={{fontSize: '10px', marginTop: '7px', color: 'gray'}}/>
+                        <p>{movieDetails.original_language}</p>
+                      </div>
+
+                      <div style={{width: '500px'}}>
+                        <h3 style={{fontFamily: "sans-serif"}}>{movieDetails.title || movieDetails.name}</h3>
+                        <br/>
+                        <p style={{fontStyle: 'oblique'}}>{movieDetails.overview}</p>
+                      </div>
+              
+
+                      <div style={{display: 'flex', gap: '20px'}}>
+                        <button 
+                        onClick={() => PlayVideo(movieDetails.title || movieDetails.name)}
+                        style={{backgroundColor: 'red'}} 
+                        className='selected-filter'> 
+                          <FaPlay style={{fontSize: '10px', marginBottom: '2px', marginRight: '5px'}}/> 
+                          Watch
+                        </button> 
+                        <button className='default-filter' onClick={() => onRenderWatchlist(movieDetails)}> <FiPlusCircle style={{fontSize: '15px', marginBottom: '2px', marginRight: '5px'}}/> Add to Watchlist</button> 
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+
+            {showVideo && (
+              <div className="video-overlay" style={{position: 'absolute', top: "0"}}>
+                <iframe
+                  title="YouTube Video"
+                  width="1770px"
+                  height="500px"
+                  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                  allowFullScreen
+                  allow="autoplay"
+                ></iframe> 
+                <h3 onClick={EndVideo} style={{color: "white", position: 'absolute', top: "5px", right: "235px"}}><AiOutlineClose /></h3>
+                <p style={{color: "white", position: 'absolute', top: "37px", right: "230px"}}>Close</p>
+
+              </div>
+            )}
+            
+          </div>
           
           <div id='categories'>
-            <button class="btn warning">TV Shows</button>
-            <button class="btn warning">Movies</button>
+            <p className={activeCategory === 'all' ? 'selected-category' : ''} onClick={() => filterByCategory('all')}> All </p>
+            <p className={activeCategory === 'movie' ? 'selected-category' : ''} onClick={() => filterByCategory('movie')}> Movies </p>
+            <p className={activeCategory === 'tv' ? 'selected-category' : ''} onClick={() => filterByCategory('tv')}> TV Shows</p>
           </div>
 
           <div id='filters'>
-            <p>Trending</p>
-            <p>Popular</p>
-            <p>Top Rated</p>
-            <p>Upcoming</p>
+            <button className={activeFilter === 'new' ? 'selected-filter' : 'default-filter'} onClick={() => filters('new')}> New </button>
+            <button className={activeFilter === 'popular' ? 'selected-filter' : 'default-filter'} onClick={() => filters('popular')}> Popular </button>
+            <button className={activeFilter === 'top_rated' ? 'selected-filter' : 'default-filter'} onClick={() => filters('top_rated')}> Top Rated </button>
+            <button className={activeFilter === 'upcoming' ? 'selected-filter' : 'default-filter'} onClick={() => filters('upcoming')}> Upcoming </button> 
           </div>
        
           <div className='films'>
-          {All.map(film => (
-            <div key={film.id} style={{width: "200px", padding: "10px"}} 
-            // onClick={() => viewDetails(film.id)}
-            >
-              <img alt = {film.title} style={{width: "150px"}} src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}/>
-              <figcaption style={{justifyContent: "center", wordWrap: "break-word"}}>{film.title || film.name}</figcaption>
-            </div>
-          ))}
+            {filtered_display?.map(film => (
+              <div key={film.id} style={{width: "200px", padding: "10px"}}>
+                <img 
+                alt = {film.title} 
+                style={{width: "150px"}} 
+                src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}
+                onClick={() => MovieDetails(film)}/>
+                <figcaption style={{justifyContent: "center", wordWrap: "break-word", fontSize: '15px'}}>{film.title || film.name}</figcaption>
+              </div>
+            ))}
           </div>
 
         </div>
+      
       </div>
     </>
-      
-    
-      
-        
-        
-
-
-
-
-        
-  )
+  );
 }
 
-export default Home
+export default Home;
