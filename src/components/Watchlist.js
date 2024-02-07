df4fefd27ac68da0f7845c797d4a3e8f511b2202
Ron Maina <ronmwangi00@gmail.com>
2024-02-08 00:10:46 +0300

    Render watchlist for logged in user

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,33 +1,61 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import Sidebar from './Sidebar'
 import NavBar from './NavBar'
-import Button from 'react-bootstrap/Button';
-import { AiFillDelete } from 'react-icons/ai';
 
-function Watchlist({AddedWatchlist, onRemove}) {
 
-    function deleteMovie(id){
-        fetch(`http://localhost:3002/watchlist/${id}`, {
-            method: "DELETE"
-        })
-        onRemove(id)
-    }
+function Watchlist() {
+
+    const id = localStorage.getItem('user-id');
+
+    const [myMovies, setMyMovies] = useState()
+
+
+    useEffect(() => {
+        fetch(`/user/${id}`)
+        .then(res => res.json())
+        .then(data => setMyMovies(data.my_watchlist))
+    }, [id])
+
+    console.log(myMovies)
+    
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: "auto 1fr"}}>
-            <Sidebar />
-            <div style={{height: '100vh'}}>
-                <NavBar />
-                <div  style={{display: "flex", flexWrap: "wrap", justifyContent: "normal"}}>
-                    {AddedWatchlist.map(movie => (
-                        <div key={movie.id} style={{width: "200px", padding: "10px", position: 'relative'}}>
-                            <img alt = {movie.title || movie.name} style={{width: "150px"}} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
-                            <figcaption style={{justifyContent: "center", wordWrap: "break-word"}}>{movie.title || movie.name}</figcaption>
-                            <h4 onClick = {() => deleteMovie(movie.id)} style={{color: "white", position: 'absolute', top: "-5px", right: "30px"}}><AiFillDelete/></h4>
-                        </div>
-                    ))}
-                </div>
+        <>
+        <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr' }} className='app-page'>
+          <Sidebar />
+  
+          <div style={{height: "100vh", overflow: "hidden"}} >
+            <NavBar/>
+
+           
+  
+            <div className='watchlist'>
+                {myMovies?.length 
+                    ? (
+                        <>
+                            <div className='films'>
+                                {myMovies.map(film => (
+                                <div key={film.id} style={{width: "200px", padding: "10px", height: '300px'}}>
+                                    <img 
+                                    alt = {film.title} 
+                                    style={{width: "150px"}} 
+                                    src={`https://image.tmdb.org/t/p/w500/${film.poster}`}
+                                    />
+                                    <figcaption style={{justifyContent: "center", wordWrap: "break-word", fontSize: '15px'}}>{film.title}</figcaption>
+                                </div>
+                                ))}
+                            </div>
+                        </>
+                    ) :
+                        <p style={{textAlign: 'center'}}>No Movies to Display</p>
+                }
+              
             </div>
+            
+            
+          </div>
+        
         </div>
+      </>
     )
 }
 
