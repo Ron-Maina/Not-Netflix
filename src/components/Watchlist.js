e2551ef9dd1f6e1aa1f441a448814724e9e1cc04
Ron Maina <ronmwangi00@gmail.com>
2024-02-08 16:13:30 +0300

    Added delete functionality

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,22 +1,77 @@
 import {useEffect, useState} from 'react'
 import Sidebar from './Sidebar'
 import NavBar from './NavBar'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+
 
 
 function Watchlist() {
 
-    const id = localStorage.getItem('user-id');
+    const user_id = localStorage.getItem('user-id');
+    const token = localStorage.getItem('jwt-token');
+
 
     const [myMovies, setMyMovies] = useState()
+    const [show, setShow] = useState(false);
+    const [filmDetails, setDetails] = useState(false);
 
+    const [successful, setSuccessful] = useState(false)
+    const [failed, setFailed] = useState(false)
 
     useEffect(() => {
-        fetch(`/user/${id}`)
+        fetch(`/user/${user_id}`)
         .then(res => res.json())
-        .then(data => setMyMovies(data.my_watchlist))
-    }, [id])
+        .then(data => {
+            setMyMovies(data.my_watchlist)})
+    }, [user_id])
 
-    console.log(myMovies)
+
+    const handleClose = () => setShow(false);
+    const handleShow = (film) => {
+        setShow(true);
+        setDetails(film)
+    }
+
+    const handleDelete = (movie_id) => {
+        console.log(movie_id)
+
+        fetch(`/delete/${movie_id}/${user_id}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",   
+                'Authorization': 'Bearer ' + token,
+            },
+        })
+        .then((response) => {
+            if (!response.ok) {
+                setFailed(true)
+                handleClose()
+            } else {
+                setSuccessful(true)
+                handleClose()
+                setMyMovies(myMovies?.filter(movie => movie.movie_id !== movie_id))    
+            }   
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+        
+    }
+
+
+    setTimeout(() => {
+        setSuccessful(false)
+    }, 1000);
+    
+    setTimeout(() => {
+    setFailed(false)
+    }, 1000);
+
     
     return (
         <>
@@ -25,17 +80,49 @@ function Watchlist() {
   
           <div style={{height: "100vh", overflow: "hidden"}} >
             <NavBar/>
+            {successful && (
+              <div className='watchlist-alert-box'>
+                <Alert variant={'success'} className='alert'>
+                  Deleted Successfully
+                </Alert>
+              </div>
+            )}
 
+            {failed && (
+              <div className='watchlist-alert-box'>
+                <Alert variant={'danger'} className='alert'>
+                  Failed to Delete
+                </Alert>
+              </div>
+            )}
            
-  
+            <Modal 
+                className="my-modal" 
+                show={show} 
+                onHide={handleClose}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton style={{color:'white'}}>
+                <Modal.Title>{filmDetails.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{filmDetails.overview}</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={() => handleDelete(filmDetails.movie_id)}>
+                    Delete
+                </Button>
+                </Modal.Footer>
+            </Modal>
             <div className='watchlist'>
                 {myMovies?.length 
                     ? (
                         <>
                             <div className='films'>
                                 {myMovies.map(film => (
-                                <div key={film.id} style={{width: "200px", padding: "10px", height: '300px'}}>
+                                <div key={film.movie_id} style={{width: "200px", padding: "10px", height: '300px'}}>
                                     <img 
+                                    onClick={() => handleShow(film)}
                                     alt = {film.title} 
                                     style={{width: "150px"}} 
                                     src={`https://image.tmdb.org/t/p/w500/${film.poster}`}
